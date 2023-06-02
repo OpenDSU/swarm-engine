dc17bdcfb201e1f6f88e943bf6177f66a9542d11
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2023-06-02 17:50:36 +0300

    cleanup

--- bootScripts/browser/versionless/index.js
+++ bootScripts/browser/versionless/index.js
@@ -2,30 +2,40 @@ const {getCookie, setCookie} = require("../util/cookies.js");
 const opendsu = require("opendsu");
 const keyssi = opendsu.loadApi("keyssi");
 const resolver = opendsu.loadApi("resolver");
+const swarmUtils = require("swarmutils");
 
-const cookieName = "MAIN-DSU-VALUE";
+const cookieName = "VERSIONLESS_WALLET";
 let walletSSI = getCookie(cookieName);
+let domain = undefined /*replace with  environment.domain once the versionless persistence fix*/;
 
 if(!walletSSI){
-    const path = `/dsu-explorer`;
-    resolver.createVersionlessDSU(path, (err, wallet)=>{
+    const path = `/${swarmUtils.generateUid(32).toString('hex')}`;
+    let versionLessSSI = keyssi.createVersionlessSSI(domain, path);
+    setCookie(cookieName, versionLessSSI.getIdentifier());
+    resolver.createDSUForExistingSSI(versionLessSSI, (err, wallet)=>{
         if(err){
             alert(`Not able to create Wallet using the VersionLess DSU. ${err.message}`);
             return;
         }
-        wallet.getKeySSIAsString((err, ssi)=>{
+        window.rawDossier = wallet;
+
+        const enclavePath = `/${swarmUtils.generateUid(32).toString('hex')}`;
+        environment.enclaveKeySSI = keyssi.createVersionlessSSI(domain, enclavePath);
+        wallet.writeFile("/environment.js", JSON.stringify(environment), (err)=>{
             if(err){
-                alert(`Not able to retrieve Wallet SSI. ${err.message}`);
-                return;
+                return alert(`Failed to write environment file in wallet. ${err.message}`);
             }
-            console.log("Wallet initialised. Refreshing...");
-            rawDossier = wallet;
-            setCookie(cookieName, ssi);
-            location.reload();
+            console.log("Wallet was initialised");
         });
     });
 }else{
-    resolver.loadDSU(walletSSI, (err, wallet)=>{
-        rawDossier = wallet;
-    })
+    //remove this once the persistence of versionless dsu is fixed
+    walletSSI = keyssi.parse(walletSSI).getIdentifier(true).replace("undefined", "");
+
+    resolver.loadDSU(walletSSI, (err, wallet) => {
+        if(err){
+            alert(`Unable to load Wallet`);
+        }
+        window.rawDossier = wallet;
+    });
 }
